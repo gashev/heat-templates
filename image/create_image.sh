2918cb5dd27a27ff8eaa8ec6100193bc244d0a22
Oleg Gashev <oleg@gashev.net>
2017-05-13 14:24:45 +0000

    Added script to build centos7 qcow2 image.

---
+++ image/create_image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+virtualenv .
+source bin/activate
+
+git clone https://git.openstack.org/openstack/diskimage-builder.git
+git clone https://git.openstack.org/openstack/heat-agents.git
+git clone https://git.openstack.org/openstack/tripleo-image-elements.git
+
+pip install diskimage-builder
+
+export ELEMENTS_PATH="elements:tripleo-image-elements/elements:heat-agents/:diskimage-builder/diskimage_builder/elements"
+disk-image-create \
+    vm \
+    centos7 \
+    selinux-permissive \
+    epel-install \
+    os-collect-config \
+    os-refresh-config \
+    os-apply-config \
+    dib-run-parts \
+    heat-config \
+    heat-config-script \
+    -o centos7-software-config.qcow2
+
+deactivate
